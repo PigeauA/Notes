a04b0ec0ec5530e3f94c67ac104f7b4ff32981a5
Amanda <amandapigeau@outlook.com>
2024-11-17 17:41:50 -0500

    Create lab11.sql

---
+++ lab11.sql
@@ -0,0 +1,12 @@
+-- EXERCISE QUESTIONS
+-- Assignments for a Specific Course: Write a query to display the title and due_date of assignments for the course COMP1234.
+-- Earliest Assignment Due Date: Write a query to find the earliest assignment due date in the assignments table. Hint: use SELECT min()
+-- Latest Assignment Due Date: Write a query to find the latest assignment due date in the assignments table.
+-- Assignments Due on a Specific Date: Write a query to find the title and course_id of assignments due on 2024-10-08.
+-- Find All Assignments Due in October: Write a query to display the title and due_date of assignments due in October 2024. Hint: use LIKE '2024-10%'
+-- Find the Most Recent Completed Assignment: Write a query to find the most recent due_date of assignments with a status of "Completed".
+
+-- OPTIONAL TASKS
+-- Count of "Not Started" Assignments: Write a query to find the total number of assignments with a status of "Not Started". Remember to use single quotes for strings in SQL.
+-- Find Courses with Labs on Tuesday: Write a query to find the course_id and course_name of courses that have lab sessions on Tuesday. Hint: use LIKE 'Tue%
+-- Write a query that joins the two tables: Hint -> JOIN the_other_table   ON courses.course_id = assignments.course_id
