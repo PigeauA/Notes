ce8dbd4f9c1efa65d34d677265c5d745a1cffeb6
Amanda <amandapigeau@outlook.com>
2024-11-17 20:53:28 -0500

    Update lab11.sql - Added minor touches + my [Optional 2] answer.

--- lab11.sql
+++ lab11.sql
@@ -1,33 +1,39 @@
 -- EXERCISE QUESTIONS
--- Assignments for a Specific Course: Write a query to display the title and due_date of assignments for the course COMP1234.
+-- 1. Assignments for a Specific Course: Write a query to display the title and due_date of assignments for the course COMP1234.
 SELECT title, due_date
 FROM assignments
 where course_id LIKE 'COMM%';
 
--- Earliest Assignment Due Date: Write a query to find the earliest assignment due date in the assignments table. Hint: use SELECT min()
+-- 2. Earliest Assignment Due Date: Write a query to find the earliest assignment due date in the assignments table. Hint: use SELECT min()
 SELECT min(due_date)
 FROM assignments;
 
--- Latest Assignment Due Date: Write a query to find the latest assignment due date in the assignments table.
+-- 3. Latest Assignment Due Date: Write a query to find the latest assignment due date in the assignments table.
 SELECT max(due_date)
 FROM assignments;
 
--- Assignments Due on a Specific Date: Write a query to find the title and course_id of assignments due on 2024-10-08.
+-- 4. Assignments Due on a Specific Date: Write a query to find the title and course_id of assignments due on 2024-10-08.
 SELECT title, course_id
 FROM assignments
 WHERE due_date like '2024-10-08%';
 
--- Find All Assignments Due in October: Write a query to display the title and due_date of assignments due in October 2024. Hint: use LIKE '2024-10%'
+-- 5. Find All Assignments Due in October: Write a query to display the title and due_date of assignments due in October 2024. Hint: use LIKE '2024-10%'
 SELECT title, due_date
 FROM assignments
 WHERE due_date like '2024-10%';
 
--- Find the Most Recent Completed Assignment: Write a query to find the most recent due_date of assignments with a status of "Completed".
+-- 6. Find the Most Recent Completed Assignment: Write a query to find the most recent due_date of assignments with a status of "Completed".
 SELECT max(due_date)
 FROM assignments
 WHERE status like 'Completed%';
 
 -- OPTIONAL TASKS
--- Count of "Not Started" Assignments: Write a query to find the total number of assignments with a status of "Not Started". Remember to use single quotes for strings in SQL.
--- Find Courses with Labs on Tuesday: Write a query to find the course_id and course_name of courses that have lab sessions on Tuesday. Hint: use LIKE 'Tue%
--- Write a query that joins the two tables: Hint -> JOIN the_other_table   ON courses.course_id = assignments.course_id
+-- 1. Count of "Not Started" Assignments: Write a query to find the total number of assignments with a status of "Not Started". Remember to use single quotes for strings in SQL.
+
+
+-- 2. Find Courses with Labs on Tuesday: Write a query to find the course_id and course_name of courses that have lab sessions on Tuesday. Hint: use LIKE 'Tue%
+SELECT course_id, course_name
+FROM courses
+WHERE lab_time like 'Tue%';
+
+-- 3. Write a query that joins the two tables: Hint -> JOIN the_other_table   ON courses.course_id = assignments.course_id
