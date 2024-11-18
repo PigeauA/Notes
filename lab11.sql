177eda946b05fae56ff791d357317d24f674fafc
Amanda <amandapigeau@outlook.com>
2024-11-17 20:49:40 -0500

    Updated lab11.sql - I forgot a question, but fixed all my answers.

--- lab11.sql
+++ lab11.sql
@@ -1,27 +1,31 @@
 -- EXERCISE QUESTIONS
 -- Assignments for a Specific Course: Write a query to display the title and due_date of assignments for the course COMP1234.
-SELECT min(due_date)
-FROM assignments;
+SELECT title, due_date
+FROM assignments
+where course_id LIKE 'COMM%';
+
 -- Earliest Assignment Due Date: Write a query to find the earliest assignment due date in the assignments table. Hint: use SELECT min()
-SELECT max(due_date)
+SELECT min(due_date)
 FROM assignments;
 
 -- Latest Assignment Due Date: Write a query to find the latest assignment due date in the assignments table.
+SELECT max(due_date)
+FROM assignments;
+
+-- Assignments Due on a Specific Date: Write a query to find the title and course_id of assignments due on 2024-10-08.
 SELECT title, course_id
 FROM assignments
 WHERE due_date like '2024-10-08%';
 
--- Assignments Due on a Specific Date: Write a query to find the title and course_id of assignments due on 2024-10-08.
+-- Find All Assignments Due in October: Write a query to display the title and due_date of assignments due in October 2024. Hint: use LIKE '2024-10%'
 SELECT title, due_date
 FROM assignments
 WHERE due_date like '2024-10%';
 
--- Find All Assignments Due in October: Write a query to display the title and due_date of assignments due in October 2024. Hint: use LIKE '2024-10%'
+-- Find the Most Recent Completed Assignment: Write a query to find the most recent due_date of assignments with a status of "Completed".
 SELECT max(due_date)
 FROM assignments
 WHERE status like 'Completed%';
-
--- Find the Most Recent Completed Assignment: Write a query to find the most recent due_date of assignments with a status of "Completed".
 
 -- OPTIONAL TASKS
 -- Count of "Not Started" Assignments: Write a query to find the total number of assignments with a status of "Not Started". Remember to use single quotes for strings in SQL.
